f22ab481660b784bde419698fb956be36499ee2a
Uri Stolar <uri.stolar@biis.mx>
2017-03-08 08:18:51 -0600

    Add mon présentation

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+  // response.render('pages/index');
+  res.redirect(301,'http://www.herokucdn.com/error-pages/no-such-app.html');
 });
+
+app.get('/tdf', function (req, res) {
+  res.sendFile('/tdf/index.html');
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
